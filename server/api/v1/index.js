5914859dfd3c7efb4423cc2eb26a982ea57c373c
Juan C. Moreno B <juan.carlos.moreno.banda@hotmail.com>
2021-12-12 17:57:12 -0500

    add routes for db owners ans users

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const owner =require('./owner/routes');
+const owners =require('./owners/routes');
 const users =require('./users/routes');
 
 const router = express.Router();
 
-router.use('/owner', owner);
+router.use('/owners', owners);
 router.use('/users', users);
 
 module.exports = router;
