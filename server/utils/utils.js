a50b7301dc931d8f58d76101abf45370bca42776
Juan Diego Carranza Vega <juan.carranza@smartpyme.pe>
2022-02-02 10:51:28 -0500

    Add filter cars

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -9,3 +9,20 @@ exports.filterByNested = (params, referencesNames) => {
     populate: populateNames.join(' '),
   };
 };
+
+exports.filterCarsByPrice = (data, price) =>
+  data.filter((cars) => cars.price >= price);
+exports.filterCarsByType = (data, type) =>
+  data.filter((cars) => cars.type.toLowerCase() === type);
+
+exports.filterCarsByMake = (data, make) =>
+  data.filter((cars) => {
+    let vehicleMake = make;
+    if (make.includes('_')) {
+      vehicleMake = make.replace('_', ' ');
+    }
+    return cars.make.toLowerCase() === vehicleMake;
+  });
+
+exports.filterCarsBySeats = (data, seats) =>
+  data.filter((cars) => cars.seats >= seats);
